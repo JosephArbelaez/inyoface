b38818ca2a3f471fb47209e144c50fda6b02da63
Joseph Arbelaez <josepharbelaez@gmail.com>
2018-10-23 15:03:15 -0500

    logo.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/navigation/navigation';
-
+import Logo from './components/Logo/logo'
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navigation />
+        <Logo />
       </div>
     );
   }

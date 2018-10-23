6152064016f03a399d0e74426c920b123a8aed16
Joseph Arbelaez <josepharbelaez@gmail.com>
2018-10-23 15:43:14 -0500

    Finished imageLinkform

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/navigation/navigation';
 import Logo from './components/Logo/logo'
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import ImageLinkForm from './components/imageLinkForm/imagelinkform';
 class App extends Component {
   render() {
     return (

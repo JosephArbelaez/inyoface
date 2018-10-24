f48e1d9d810dbef91d44bc24c737bebfcaab5d2c
Joseph Arbelaez <josepharbelaez@gmail.com>
2018-10-24 14:19:15 -0500

    Sign In and Register are available

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const Navigation = () => {
+// By making onRouteChange an arrow function, it allows onRouteChange to be called each time.
+const Navigation = ({onRouteChange}) => {
     return (
         <nav style={{display: 'flex', justifyContent:'flex-end'}}>
-            <p className='f3 link dim black underline pa3 pointer'>Sign out</p>
+            <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign out</p>
         </nav>
     )
 }
